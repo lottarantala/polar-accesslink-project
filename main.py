aae89bceed8c2859cf7beb422fb8afd8d5934925
Lotta Rantala <lottarantala20@gmail.com>
2025-06-04 20:59:23 +0300

    Reads accesslink information from a json file

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world")
+import json
+
+def read_accesslink_info(filepath:str) -> dict:
+    with open(filepath, 'r') as f:
+        info = json.load(f)
+    return info
+
+if __name__=="__main__":
+    accesslink_info = read_accesslink_info('accesslink_information.json')
+    print(accesslink_info)
